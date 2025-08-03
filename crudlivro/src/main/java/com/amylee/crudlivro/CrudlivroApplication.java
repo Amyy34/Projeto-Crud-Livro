1e213667a9e8324f6d1767b65634c6c708a81ec5
Amy Lee <amyleesouzatr@gmail.com>
2025-08-03 13:22:18 -0300

    feat: estrutura inicial do projeto

---
+++ crudlivro/src/main/java/com/amylee/crudlivro/CrudlivroApplication.java
@@ -0,0 +1,14 @@
+package com.amylee.crudlivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudlivroApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CrudlivroApplication.class, args);
+	}
+
+}
---
+++ crudlivro/src/test/java/com/amylee/crudlivro/CrudlivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amylee.crudlivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudlivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
